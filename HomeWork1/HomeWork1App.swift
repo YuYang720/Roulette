ade0f981bcff57087ad6d31fadd87e049c11901d
nai <terry910110@gmail.com>
2023-03-10 13:59:10 +0800

    Initial Commit

---
+++ HomeWork1/HomeWork1App.swift
@@ -0,0 +1,17 @@
+//
+//  HomeWork1App.swift
+//  HomeWork1
+//
+//  Created by User06 on 2023/3/10.
+//
+
+import SwiftUI
+
+@main
+struct HomeWork1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
